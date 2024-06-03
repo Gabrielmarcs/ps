5532f60af5142a15cea8f13b728a640e83bea5f6
Gabriel Marcos Lopes <gabriel.lopes3@estudante.ufla.br>
2024-06-03 11:44:19 -0300

    estrutura basica

---
+++ backend/src/main/java/br/com/api/backend/modelo/TipoModelo.java
@@ -0,0 +1,7 @@
+package br.com.api.backend.modelo;
+
+public class TipoModelo {
+    private long id;
+    private String descricao;
+    private String descricaoDetalhada;
+}
---
+++ backend/src/main/java/br/com/api/backend/modelo/UfModelo.java
@@ -0,0 +1,9 @@
+package br.com.api.backend.modelo;
+
+import lombok.Data;
+
+@Data
+public class UfModelo {
+    private String sigla;
+    private String nome;
+}
