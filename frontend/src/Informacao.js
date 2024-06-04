c6eb6e0547f7ff37e5c3d00c7c038942ae9d7f21
Gabriel Marcos Lopes <gabriel.lopes3@estudante.ufla.br>
2024-06-04 14:36:12 -0300

    estruturando area de informacoes

--- frontend/src/Informacao.js
+++ frontend/src/Informacao.js
@@ -1,8 +1,21 @@
 function Informacao() {
     return (
-        <div>
-            <h1>Informação</h1>
-        </div>
+        <table>
+            <thead>
+                <tr>
+                    <th>Município</th>
+                    <th>N° de pessoas beneficiadas</th>
+                    <th>Valor total</th>
+                </tr>
+            </thead>
+
+            <tbody>
+                <td></td>
+                <td></td>
+                <td></td>
+            </tbody>
+
+        </table>
     );
 }
 
